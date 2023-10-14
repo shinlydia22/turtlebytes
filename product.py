dc40ebaeccb5122f220cdaca13014a3340c1a338
shinlydia22 <143225542+shinlydia22@users.noreply.github.com>
2023-10-14 16:11:25 -0500

    commented outline for product.py

--- product.py
+++ product.py
@@ -1,2 +1,17 @@
 class Product:
-    
+    #" instance variables:
+    # string - name of product
+    # list - words extracted from image 
+    # set - hidden sugars
+    # set - carcinogens
+    # ... potentially more 
+    # "
+
+
+    #" methods:
+    # sugars - makes set w/ hidden sugars in the product
+    # carcinogens - same as sugar
+    # getSugars, getCarcinogens, getName
+    # 
+    # "
+
