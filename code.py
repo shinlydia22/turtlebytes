4d1dfc91bc4c5957464839bff0b17b65dd9f8b55
Wei Wu <128621069+weiw-w@users.noreply.github.com>
2023-10-14 14:47:38 -0500

    Create a react app

--- code.py
+++ code.py
@@ -1,1 +1,10 @@
+import os
 import easyocr
+import cv2
+# from matplotlib import pyplot as plt
+import numpy as np
+import torch
+# print(torch.cuda.is_available())
+reader = easyocr.Reader(['en'])
+result = reader.readtext(r"C:\Users\wwzb2\Downloads\water_list.jpg", paragraph=False)
+result[0:10]
