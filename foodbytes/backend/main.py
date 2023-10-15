577a6f5c6989011d021fdbdd84681213d08bf083
Karena Liang <ksliang2@illinois.edu>
2023-10-15 15:43:51 -0500

    everything

---
+++ foodbytes/backend/main.py
@@ -0,0 +1,5 @@
+from product import Product
+
+bob = Product('splenda.jpg')
+print(1+1)
+print(bob.getHiddenSugars())
---
+++ foodbytes/backend/test.py
@@ -0,0 +1,7 @@
+class Test:
+    def __init__(self):
+        self.yuh = 1
+        self.add(4,3)
+
+    def add(a, b):
+        return a + b
